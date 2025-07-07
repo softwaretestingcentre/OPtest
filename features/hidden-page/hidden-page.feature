9761d03205c8f8012dd3a658857cc785de6b29a7
NickTulett <nicktulett@yahoo.com>
2025-07-07 12:02:00 +0100

    Initial setup

--- features/hidden-page/hidden-page.feature
+++ features/hidden-page/hidden-page.feature
@@ -1,10 +1,10 @@
-Feature: Juice Shop has a hidden pages
+# Feature: Juice Shop has a hidden pages
 
-  Scenario Outline: Haxxor opens the hidden pages
-    When she opens the "<Hidden Page>"
-    Then she sees she has solved the "<Hidden Page>" challenge
+#   Scenario Outline: Haxxor opens the hidden pages
+#     When she opens the "<Hidden Page>"
+#     Then she sees she has solved the "<Hidden Page>" challenge
 
-    Examples:
-      | Hidden Page    |
-      | Score Board    |
-      | Privacy Policy |
+#     Examples:
+#       | Hidden Page    |
+#       | Score Board    |
+#       | Privacy Policy |
--- features/support/serenity.config.ts
+++ features/support/serenity.config.ts
@@ -38,8 +38,8 @@ BeforeAll(async () => {
         actors: new Actors(
             browser,
             {
-                baseURL: 'https://juice-shop.herokuapp.com/#/',//                   'https://stc-owasp-juice-dnebatcgf2ddf4cr.uksouth-01.azurewebsites.net/#/',//'http://localhost:7080/',
-                // baseURL: 'https://the-internet.herokuapp.com/',
+                // baseURL: 'https://juice-shop.herokuapp.com/#/',//                   'https://stc-owasp-juice-dnebatcgf2ddf4cr.uksouth-01.azurewebsites.net/#/',//'http://localhost:7080/',
+                baseURL: 'https://the-internet.herokuapp.com/',
             },
             {
                 defaultNavigationTimeout: timeouts.playwright.defaultNavigationTimeout.inMilliseconds(),
