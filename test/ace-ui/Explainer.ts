6ddda3babb1e0079441b0fbfa71ed693b2b338b4
NickTulett <nicktulett@yahoo.com>
2025-07-24 16:56:54 +0100

    Add some comments about testing SLA chart

--- test/ace-ui/Explainer.ts
+++ test/ace-ui/Explainer.ts
@@ -51,4 +51,19 @@ export const Explainer = {
         )
       )
     ),
+    // this would need access to the data used to create the SLA zones chart
+    // then determine whether the current and projected points lie within the expected zones
+    // // Returns true if (x, y) is inside the polygon defined by vertices
+    // export function pointInPolygon(x: number, y: number, vertices: Array<{x: number, y: number}>): boolean {
+    //   let inside = false;
+    //   for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+    //     const xi = vertices[i].x, yi = vertices[i].y;
+    //     const xj = vertices[j].x, yj = vertices[j].y;
+    //     const intersect =
+    //       ((yi > y) !== (yj > y)) &&
+    //       (x < ((xj - xi) * (y - yi)) / (yj - yi) + xi);
+    //     if (intersect) inside = !inside;
+    //   }
+    //   return inside;
+    // }
 };
